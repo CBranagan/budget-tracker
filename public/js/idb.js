dfec3fadff5fd83c8bfee0669196eeaf567fd1b6
Chad Branagan <chad.branagan@gmail.com>
2022-02-03 22:08:57 -0800

    offline transactions work and are loaded on online

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,7 +16,7 @@ request.onsuccess = function (event) {
   db = event.target.result;
 
   if (navigator.onLine) {
-    //   uploadTransaction();
+    uploadTransaction();
   }
 };
 
@@ -36,3 +36,48 @@ function saveRecord(record) {
   // add record to your store with add method
   TransactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  // access your object store
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+
+  // get all records from store and set to a variable
+  const getAll = transactionObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          // access the new_transaction object store
+          const transactionObjectStore =
+            transaction.objectStore("new_transaction");
+          // clear all items in your store
+          transactionObjectStore.clear();
+
+          alert("All saved transactions has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadTransaction);
